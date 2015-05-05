913735ffb3ee118423e41f8ba728a4ba5746aebf
GokuMizuno <Erfworld03>
2015-05-04 21:30:12 -0400

    shell 	new file:   ../CSForms.sln 	new file:   App.config 	new file:   CSForms.csproj 	new file:   Form1.Designer.cs 	new file:   Form1.cs 	new file:   Form1.resx 	new file:   Program.cs 	new file:   Properties/AssemblyInfo.cs 	new file:   Properties/Resources.Designer.cs 	new file:   Properties/Resources.resx 	new file:   Properties/Settings.Designer.cs 	new file:   Properties/Settings.settings

---
+++ CSForms/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CSForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTempF = new System.Windows.Forms.TextBox();
+            this.tbTempC = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbTempF
+            // 
+            this.tbTempF.Location = new System.Drawing.Point(36, 57);
+            this.tbTempF.Name = "tbTempF";
+            this.tbTempF.Size = new System.Drawing.Size(100, 20);
+            this.tbTempF.TabIndex = 0;
+            // 
+            // tbTempC
+            // 
+            this.tbTempC.Location = new System.Drawing.Point(36, 96);
+            this.tbTempC.Name = "tbTempC";
+            this.tbTempC.Size = new System.Drawing.Size(100, 20);
+            this.tbTempC.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(36, 134);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Degrees F";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Degrees C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.tbTempC);
+            this.Controls.Add(this.tbTempF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTempF;
+        private System.Windows.Forms.TextBox tbTempC;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CSForms/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double temp;
+            //converts from F to C
+            //Checks to see if F is empty.
+            //TryParse checks to see if input is valid.
+            bool parsesuccessF = false;
+            bool parsesuccessC = false;
+            parsesuccessF = Double.TryParse(tbTempF.Text, out temp);
+            parsesuccessC = Double.TryParse(tbTempC.Text, out parsesuccessC);
+            try
+            {
+                if (tbTempF.Text != String.Empty)
+                {
+                    temp = Convert.ToDouble(tbTempF.Text) - 32;
+                    temp /= 9.0;
+                    temp *= 5.0;
+                    tbTempC.Text = Convert.ToString(temp);
+                    tbTempC.Text = String.Format("{0:N2}", temp);
+                }
+                else if (tbTempC.Text != String.Empty)
+                {
+                    temp = Convert.ToDouble(tbTempC.Text) + 32;
+                    temp *= 9.0;
+                    temp /= 5.0;
+                    tbTempF.Text = Convert.ToString(temp);
+                    tbTempF.Text = String.Format("}0:N2}", temp);
+                }
+                else if (tbTempF.Text == String.Empty && tbTempC.Text == String.Empty)
+                {
+                    MessageBox.Show("Please fill out either the F or C scale with a valid number.\n");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
